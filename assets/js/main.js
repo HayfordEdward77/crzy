b0590071ae0dacac196b094289769ce5e7095aea
HayfordEdward77 <edwardaddo77@gmail.com>
2020-11-10 13:57:02 +0000

    minor changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,3 +44,28 @@ const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
 tl.to(".slider", { y: "-100%", duration: 1.3, delay: 0.5 });
 tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
+
+
+// ==============================================================
+//                          Cursor Animation
+// ==============================================================
+let mouseCursor = document.querySelector('.cursor');
+let navLinks = document.querySelectorAll('a');
+
+window.addEventListener("mousemove",cursor);
+
+function cursor(e){
+    mouseCursor.style.top = e.pageY + 'px';
+    mouseCursor.style.left = e.pageX + 'px';
+}
+
+navLinks.forEach(link =>{
+    link.addEventListener("mouseleave", () => {
+        mouseCursor.classList.remove("link-grow");
+        link.classList.remove("hovered-link");
+    });
+    link.addEventListener("mouseover", () => {
+        mouseCursor.classList.add("link-grow");
+        link.classList.add("hovered-link");
+    });
+});
