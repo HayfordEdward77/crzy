e3c8e7ae39b37480934eff8b712a902a433ce2fe
HayfordEdward77 <edwardaddo77@gmail.com>
2020-11-10 08:13:51 +0000

    minor changed made

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,5 +42,5 @@ linkColor.forEach( l => l.addEventListener('click',colorLink))
 const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
 tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
-tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
+tl.to(".slider", { y: "-100%", duration: 1.3, delay: 0.5 });
 tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
